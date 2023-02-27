96114d1312204097c8a9622add82ff682689ce18
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-02-27 15:44:12 +0900

    update

---
+++ nim/build-nimble-package.nix
@@ -0,0 +1,60 @@
+{ nim
+, stdenv
+, runCommand
+, nimble
+, lib
+, git
+, unixtools
+, cacert
+, breakpointHook
+}:
+
+{ src
+, vendorHash ? lib.fakeHash
+, pname
+, nimbleFileName ? "${pname}.nimble"
+, ...
+} @ args:
+
+let
+  vendor = runCommand "vendor" {
+    outputHashAlgo = null;
+    outputHash = vendorHash;
+    outputHashMode = "recursive";
+    lockFile = src + "/nimble.lock";
+    nimbleFile = src + "/${nimbleFileName}";
+    nativeBuildInputs = [ git nimble nim unixtools.ping ];
+    GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ [
+      "GIT_PROXY_COMMAND" "NIX_GIT_SSL_CAINFO" "SOCKS_SERVER"
+    ];
+  } ''
+    cp $nimbleFile ${pname}.nimble
+    cp $lockFile nimble.lock
+    nimble --nimbleDir:nimbledeps install --depsOnly
+    mkdir -p $out
+    cp -a nimbledeps/pkgs2 $out/pkgs2
+  '';
+in
+stdenv.mkDerivation (args // {
+  depsBuildBuild = [ nim ];
+  nativeBuildInputs = [ nimble ];
+
+  buildPhase = ''
+    nimbledeps=/tmp/nimbledeps
+    mkdir -p $nimbledeps/pkgs2
+    for pkg in ${vendor}/pkgs2/*; do
+      ln -s $pkg $nimbledeps/pkgs2
+    done
+    ls -l
+    # ls -l $nimbledeps/pkgs2
+    # nimble build --nimbleDir:$nimbledeps --nimcache:/tmp/nimcache
+    nimble install --nimbleDir:$nimbledeps --verbose --nimcache:/tmp/nimcache
+
+    for pkg in $nimbledeps/pkgs2/*; do
+      if [[ ! -L $pkg ]]; then
+        cp -a $pkg $out
+      fi
+    done 
+  '';
+})
